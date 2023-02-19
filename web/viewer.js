ab89c08996c66a1b0398eed9aca3f6839a2cf7d0
Anshul Gupta <anshulgupta0803@gmail.com>
2023-02-19 18:36:34 +0000

    Fixing relative paths
    Signed-off-by: Anshul Gupta <anshulgupta0803@gmail.com>

--- web/viewer.js
+++ web/viewer.js
@@ -753,7 +753,7 @@ const defaultOptions = {
     kind: OptionKind.WORKER
   },
   workerSrc: {
-    value: "../build/pdf.worker.js",
+    value: "build/pdf.worker.js",
     kind: OptionKind.WORKER
   }
 };
